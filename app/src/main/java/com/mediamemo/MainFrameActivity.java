cc018c094db75f0b8bcfca5b763e6ae41c8fe94f
ash <ashye@vip.qq.com>
2015-12-08 11:12:03 +0800

    初始导入

---
+++ app/src/main/java/com/mediamemo/MainFrameActivity.java
@@ -0,0 +1,13 @@
+package com.mediamemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainFrameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_frame);
+    }
+}
